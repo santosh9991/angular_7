f25bf20bf96b39d86f5f42c57c180543edbb7adb
santosh kesireddy <kesireddy.santosh@ibm.com>
2019-04-04 17:52:26 -0500

    Working with component templates

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,16 @@
+// A component is simply a class so that angular can instantiate it based on the blueprint  we create here
+//@Component is a decorator which tells ts that it is not a regular class but a component angular class
+//decorater enhances the functionality of the class
+//As js is not aware of Components, we need to import from @angular/core
+import { Component } from '@angular/core'
+
+@Component(
+    //configuring the component. Meta data which tells angular what to do wd this meta data
+    //selecter:html tag which you are going to use later in app. It should be string, unique
+    {selector : 'app-server',
+    //relative path should be specified
+    templateUrl:'./server.component.html'
+    })
+export class ServerComponent{
+
+}
