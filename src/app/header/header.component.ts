296117beaec57da2b9b380b55e1b56632721f9c2
santosh9991 <santu1santosh@gmail.com>
2019-04-08 14:31:52 -0500

    App header added

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    //selector helps us use this component
+    selector:'app-header',
+    templateUrl:'./header.component.html'
+}
+)
+export class HeaderComponent {
+
+}
