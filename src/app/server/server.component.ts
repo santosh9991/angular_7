529171582453817d609f001ef303f051863bb359
santosh kesireddy <kesireddy.santosh@ibm.com>
2019-06-21 22:38:30 -0500

    Angular lecture 37: styling elements dynamically using ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,8 +8,16 @@ import { Component } from '@angular/core'
     //configuring the component. Meta data which tells angular what to do wd this meta data
     //selecter:html tag which you are going to use later in app. It should be string, unique
     {selector : 'app-server',
-    //relative path should be specified
+    /*relative path should be specified when templateUrl proprtty is used
+    we could also atcheve the same functionality by following inline approach where you specify the html inside ``.
+    template:`<h3>inline approach to template definition</h3>`
+    */
+
     templateUrl:'./server.component.html',
+    /*
+        styles parameter could also br defined with path as follows
+        styleUrls:['./server.component.css'], if css is present the current directory
+    */
     styles:[`
         .online{
             color:white;
@@ -21,6 +29,7 @@ export class ServerComponent{
     serverId = 10;
     serverStatus = 'offline';
     constructor(){
+        //constructor is loaded first before rest of the ServeComponent code. It is called as soon as the component is called
         this.serverStatus = Math.random()>0.5?'online' : 'offline'
     }
     getServerStatus(){
